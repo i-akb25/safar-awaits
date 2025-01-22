25951e5e26ea0e4cdaa210d2e50bdb8f389f167b
ANURAG ARYAN <aryananurag1025@gmail.com>
2025-01-22 21:43:04 +0530

    fixed project

--- src/components/Common/Header/Header.jsx
+++ src/components/Common/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import {
   Container,
-  Row,
   Navbar,
   Offcanvas,
   Nav,
@@ -17,94 +16,84 @@ const Header = () => {
     setOpen(!open);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     window.addEventListener("scroll", isSticky);
-    return ()=>{
+    return () => {
       window.removeEventListener("scroll", isSticky)
     }
   })
 
   // sticky Header 
-  const isSticky=(e)=>{
+  const isSticky = (e) => {
     const header = document.querySelector('.header-section');
     const scrollTop = window.scrollY;
     scrollTop >= 120 ? header.classList.add('is-sticky') :
-    header.classList.remove('is-sticky')
+      header.classList.remove('is-sticky')
   }
 
-
- 
-
   return (
-    
     <header className="header-section">
       <Container>
-       
-          <Navbar expand="lg" className="p-0">
-            {/* Logo Section  */}
-            <Navbar.Brand>
-              <NavLink to="/">Safar Awaits</NavLink>
-            </Navbar.Brand>
-            {/* End Logo Section  */}
+        <Navbar expand="lg" className="p-0">
+          {/* Logo Section  */}
+          <Navbar.Brand>
+            <NavLink to="/">Safar Awaits</NavLink>
+          </Navbar.Brand>
+          {/* End Logo Section  */}
 
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-lg`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
-              placement="start"
-              show={open}
-            >
-              {/*mobile Logo Section  */}
-              <Offcanvas.Header>
-                <h1 className="logo">Safar Awaits</h1>
-                <span className="navbar-toggler ms-auto"  onClick={toggleMenu}>
-                  <i className="bi bi-x-lg"></i>
-                </span>
-              </Offcanvas.Header>
-              {/*end mobile Logo Section  */}
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-lg`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+            placement="start"
+            show={open}
+          >
+            {/*mobile Logo Section  */}
+            <Offcanvas.Header>
+              <h1 className="logo">Safar Awaits</h1>
+              <span className="navbar-toggler ms-auto" onClick={toggleMenu}>
+                <i className="bi bi-x-lg"></i>
+              </span>
+            </Offcanvas.Header>
+            {/*end mobile Logo Section  */}
 
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <NavLink className="nav-link" to="/" >
-                    Home
-                  </NavLink>
-                  <NavLink className="nav-link" to="/" >
-                    ABOUT US
-                  </NavLink>
-                  <NavLink className="nav-link" to="/" >
-                    TOURS
-                  </NavLink>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <NavLink className="nav-link" to="/" >
+                  Home
+                </NavLink>
+                <NavLink className="nav-link" to="/" >
+                  ABOUT US
+                </NavLink>
+                <NavLink className="nav-link" to="/" >
+                  TOURS
+                </NavLink>
 
-                  <NavDropdown
-                    title="DESTINATION"
-                    id={`offcanvasNavbarDropdown-expand-lg`}
-                  >
-                   
-                      
-                    <NavLink className="nav-link text-dark" to="/" >
+                <NavDropdown
+                  title="DESTINATION"
+                  id={`offcanvasNavbarDropdown-expand-lg`}
+                >
+                  <NavLink className="nav-link text-dark" to="/" >
                     SPAIN TOURS
                   </NavLink>
-                  
-                   
-                  </NavDropdown>
-                  <NavLink className="nav-link" to="/" >
-                    GALLERY
-                  </NavLink>
-                  <NavLink className="nav-link" to="/" >
-                    CONTACT
-                  </NavLink>
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-            <div className="ms-md-4 ms-2">
-              <NavLink className="primaryBtn d-none d-sm-inline-block">
-                Book Now
-              </NavLink>
-              <li className="d-inline-block d-lg-none ms-3 toggle_btn">
-                <i className={open ? "bi bi-x-lg" : "bi bi-list"}  onClick={toggleMenu}></i>
-              </li>
-            </div>
-          </Navbar>
-    
+                </NavDropdown>
+                <NavLink className="nav-link" to="/" >
+                  GALLERY
+                </NavLink>
+                <NavLink className="nav-link" to="/" >
+                  CONTACT
+                </NavLink>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+          <div className="ms-md-4 ms-2">
+            <NavLink className="primaryBtn d-none d-sm-inline-block">
+              Book Now
+            </NavLink>
+            <li className="d-inline-block d-lg-none ms-3 toggle_btn">
+              <i className={open ? "bi bi-x-lg" : "bi bi-list"} onClick={toggleMenu}></i>
+            </li>
+          </div>
+        </Navbar>
       </Container>
     </header>
   );
